65c2ce65b4ebf88692b1cbb4285edf5a3718024a
Wall <307606056@qq.com>
2019-07-04 09:03:02 +0800

    添加阿里云oss代码

--- amqp/client.go
+++ amqp/client.go
@@ -1,4 +1,25 @@
 package amqp
 
-type client interface {
+import (
+	"github.com/streadway/amqp"
+	"sync"
+)
+
+// RabbitMQ 用于管理和维护rabbitmq的对象
+type RabbitMQ struct {
+	wg sync.WaitGroup
+
+	channel      *amqp.Channel
+	exchangeName string // exchange的名称
+	exchangeType string // exchange的类型
+	receivers    []Receiver
+}
+
+// New 创建一个新的操作RabbitMQ的对象
+func New() *RabbitMQ {
+	// 这里可以根据自己的需要去定义
+	return &RabbitMQ{
+		exchangeName: ExchangeName,
+		exchangeType: ExchangeType,
+	}
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/coreos/etcd v3.3.13+incompatible // indirect
 	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94 // indirect
 )
